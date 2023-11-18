47e56502e0a46b41058019252263f32f2831440b
Sparrow0hawk <alexjcoleman@gmail.com>
2023-11-18 21:41:33 +0000

    Add page not found default service

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,3 +1,5 @@
 mod index;
+mod not_found;
 
 pub use index::*;
+pub use not_found::*;
--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,6 @@
 use std::net::TcpListener;
 
-use crate::routes::{index};
+use crate::routes::{index, not_found};
 
 use actix_web::dev::Server;
 use actix_web::middleware::Logger;
@@ -13,6 +13,9 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
             .route("/", web::get().to(index))
             .wrap(Logger::default())
             .wrap(Logger::new("%a %{User-Agent}i"))
+            .default_service(
+                web::get().to(not_found)
+            )
     })
         .listen(listener)?
         .run();
